8abcc46895cdab1b588cc015ca69e110c706f82f
Nikhil-Raghav <nikhilraghavvb20@gmail.com>
2026-02-11 19:56:32 +0530

    convert dev folder to normal directory

---
+++ env/dev/main.tf
@@ -0,0 +1,16 @@
+module "vpc" {
+    source = "../../modules/vpc"
+    vpc_cidr = var.vpc_cidr
+    public_subnet_cidr = var.public_subnet_cidr
+    private_subnet_cidr = var.private_subnet_cidr
+    region = var.region
+    common_tags         = var.common_tags
+}
+
+module "ec2" {
+  source = "../../modules/ec2"
+  subnet_id     = module.vpc.public_subnet_id
+  vpc_id        = module.vpc.vpc_id
+  ami           = var.ami
+  instance_type = var.instance_type
+}
---
+++ env/dev/terraform.tfvars
@@ -0,0 +1,16 @@
+region = "us-east-1"
+
+vpc_cidr = "10.10.0.0/16"
+public_subnet_cidr = "10.10.1.0/24"
+private_subnet_cidr = "10.10.2.0/24"
+
+
+ami = "ami-0c02fb55956c7d316"
+instance_type = "t2.micro"
+
+common_tags = {
+  Environment = "dev"
+  Project     = "modular-vpc"
+  Owner       = "nikhil"
+}
+
